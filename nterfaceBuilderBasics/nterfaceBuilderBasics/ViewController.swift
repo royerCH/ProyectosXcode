2fb5a9b76d86c56bd3d9065e737e4cb2cb4f03cd
royerCH <royerch_6@hotmail.com>
2023-03-31 05:20:16 -0600

    tercer commit

--- nterfaceBuilderBasics/nterfaceBuilderBasics/ViewController.swift
+++ nterfaceBuilderBasics/nterfaceBuilderBasics/ViewController.swift
@@ -12,7 +12,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let label = UILabel(frame: CGRect(x: 16, y: 16, width: 200,
+        height: 44))
+        view.addSubview(label)// Agrega una etiqueta como vista
+        
     }
+    
+    
 
 
 }
