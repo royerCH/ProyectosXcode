0a6607093348eaa44d7982dcf143b4c082bcc69e
royerCH <royerch_6@hotmail.com>
2023-03-31 06:30:04 -0600

    Cambio de Idioma

--- nterfaceBuilderBasics/nterfaceBuilderBasics/ViewController.swift
+++ nterfaceBuilderBasics/nterfaceBuilderBasics/ViewController.swift
@@ -8,17 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+ 
+    @IBOutlet weak var viewContent: UIView!
+    
+    @IBOutlet weak var myFirdstButton: UIButton!
+    
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let label = UILabel(frame: CGRect(x: 16, y: 16, width: 200,
+        viewContent.backgroundColor = .systemOrange
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 350, width: 400,
         height: 44))
         view.addSubview(label)// Agrega una etiqueta como vista
         
+        label.text = "I'm learning how to make really awesome apps!"
+        label.textColor = .black
+        label.isEnabled = true
+        label.adjustsFontForContentSizeCategory = false
+        label.font = UIFont(name: "Arial", size: 25)
+        label.textAlignment = .justified
+        label.numberOfLines = 2
+        label.adjustsFontSizeToFitWidth = true
+        
+        
+        
+        // cambiar a idioma
+        
+        myFirdstButton.setTitle("Traducir", for: .normal)
+    
     }
     
+    @IBAction func changingLanguaje(_ sender: Any) {
+        
+        mainLabel.text = "¡Estoy aprendiendo cómo hacer aplicaciones realmente asombrosas!"
+        mainLabel.numberOfLines = 2
+        mainLabel.font = UIFont(name: "Arial", size: 22)
+        
+        print("Voy a traducir")
+    }
     
 
 
